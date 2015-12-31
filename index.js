537c0d8df6de87244a4ba211a44cdda5b549511d
Tushar Mathur <tusharmath@gmail.com>
2015-12-31 13:52:48 +0530

    add tests

--- index.js
+++ index.js
@@ -6,17 +6,18 @@
 
 const BehaviorSubject = require('rx').BehaviorSubject
 
-exports.createStoreAsStream = value => {
+exports.createStoreAsStream = (value, options) => {
   const stream = new BehaviorSubject(value)
   return {
     getStream: () => stream,
     update: cb => {
-      const _value = cb(value)
+      const _value = typeof cb === 'function' ? cb(value) : cb
       if (value !== _value) {
         value = _value
         stream.onNext(value)
       }
       return stream
+    },
     }
   }
 }
---
+++ test.js
@@ -0,0 +1,35 @@
+/**
+ * Created by tushar.mathur on 31/12/15.
+ */
+
+'use strict'
+
+import test from 'ava'
+import {createStoreAsStream} from './'
+
+test('update(function)', t => {
+  const out = []
+  const store = createStoreAsStream(100)
+  store.getStream().subscribe(x => out.push(x))
+  store.update(x => 200)
+  store.update(x => 300)
+  t.same(out, [100, 200, 300])
+})
+
+test('update(value)', t => {
+  const out = []
+  const store = createStoreAsStream(100)
+  store.getStream().subscribe(x => out.push(x))
+  store.update(200)
+  store.update(300)
+  t.same(out, [100, 200, 300])
+})
+
+test('update(value):distinct', t => {
+  const out = []
+  const store = createStoreAsStream(100)
+  store.getStream().subscribe(x => out.push(x))
+  store.update(200)
+  store.update(200)
+  t.same(out, [100, 200])
+})
