b0a7e8960b85b0e0833be4b517310d9e11b1a925
Tushar Mathur <tusharmath@gmail.com>
2016-01-08 12:30:49 +0530

    add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+remote="https://$WERCKER_TOKEN@github.com/tusharmath/reactive-storage.git"
+
+git config user.email "tusharmath@gmail.com"
+git config user.name "werckerbot"
+
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+npm version prerelease
+git push --all
+npm publish
+
