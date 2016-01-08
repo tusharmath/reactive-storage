8f9ab20d0e226f07859f811598d9a48aea307e43
Tushar Mathur <tusharmath@gmail.com>
2016-01-08 17:06:06 +0530

    echo wercker_started_by

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 REMOTE_URL="https://$GH_TOKEN@github.com/tusharmath/reactive-storage.git"
 
+echo $WERCKER_STARTED_BY
 git config user.email "pleasemailus@wercker.com"
 git config user.name "werckerbot"
 git config push.default matching
 
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-npm version $NPM_RELEASE -m "$NPM_RELEASE %s [skip ci]"
+npm version $NPM_RELEASE
 git push $REMOTE_URL HEAD:$WERCKER_GIT_BRANCH --tags
 npm publish
 
