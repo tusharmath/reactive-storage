58a277077a22d529b37d07e07ec72455051adef0
Tushar Mathur <tusharmath@gmail.com>
2016-01-08 13:39:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ git config push.default matching
 
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 npm version $NPM_RELEASE
-git push --all --verbose --repo=$REMOTE_URL
+git push --all --verbose --repo=$REMOTE_URL develop:develop
 npm publish
 
