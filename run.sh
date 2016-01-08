ced1c1248ce2b4bf55df1e2c9f7ed71c268f041b
Tushar Mathur <tusharmath@gmail.com>
2016-01-08 13:17:54 +0530

    add git push config

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ REMOTE_URL="https://$GH_TOKEN@github.com/tusharmath/reactive-storage.git"
 
 git config user.email "pleasemailus@wercker.com"
 git config user.name "werckerbot"
+git config push.default matching
 
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 npm version $NPM_RELEASE
