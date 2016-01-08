53cd3172cd3d948527561691a4cb7c149377d812
Tushar Mathur <tusharmath@gmail.com>
2016-01-08 15:39:51 +0530

    Revert "fix #3"
    This reverts commit e97628118f3935ff09402f817c1b53ef709fb133.

--- index.js
+++ index.js
@@ -11,19 +11,39 @@ exports.createStoreStream = (value) => {
   if (value === undefined) {
     value = ignoredValues
   }
+  var UNDO_HISTORY = []
+  var REDO_HISTORY = []
 
   const stream = new BehaviorSubject(value)
-  const dispatchValue = _value => {
+  const dispatchValue = (_value, push) => {
     var valueIsDefined = _value !== undefined
     var valueIsDifferent = value !== _value
+    var pushIsTrue = push === true
+
+    if (valueIsDefined && push === false) {
+      REDO_HISTORY.push(value)
+    }
+
     if (valueIsDefined && valueIsDifferent) {
+      if (pushIsTrue && value !== ignoredValues) {
+        UNDO_HISTORY.push(value)
+      }
       value = _value
       stream.onNext(value)
     }
   }
   return {
     getStream: () => stream.filter(x => x !== ignoredValues),
-    set: cb => dispatchValue(typeof cb === 'function' ? cb(value) : cb, true),
-    get: () => value
+    update: cb => {
+      REDO_HISTORY = []
+      dispatchValue(typeof cb === 'function' ? cb(value) : cb, true)
+      return stream
+    },
+    undo: () => {
+      dispatchValue(UNDO_HISTORY.pop(), false)
+    },
+    redo: () => {
+      dispatchValue(REDO_HISTORY.pop(), true)
+    }
   }
 }
--- test.js
+++ test.js
@@ -43,13 +43,66 @@ test('update(value):distinct', t => {
   t.same(out, [100, 200])
 })
 
-test('update(undefined)', t => {
+test('undo():single', t => {
   const out = []
   const store = createStoreStream(100)
   store.getStream().subscribe(x => out.push(x))
-  store.set(200)
-  store.set()
-  store.set()
-  store.set(300)
-  t.same(out, [100, 200, 300])
+  store.update(200)
+  store.update(300)
+  store.update(400)
+  store.undo()
+  t.same(out, [100, 200, 300, 400, 300])
+})
+
+test('undo()', t => {
+  const out = []
+  const store = createStoreAsStream(100)
+  store.getStream().subscribe(x => out.push(x))
+  store.update(200)
+  store.update(300)
+  store.update(400)
+  store.undo()
+  store.undo()
+  store.undo()
+  store.undo()
+  store.undo()
+  t.same(out, [100, 200, 300, 400, 300, 200, 100])
+})
+
+test('redo+undo', t => {
+  const out = []
+  const store = createStoreAsStream()
+  store.getStream().subscribe(x => out.push(x))
+  store.update(100) // 100
+  store.update(200) // 200
+  store.update(300) // 300
+  store.undo() // 200
+  store.undo() // 100
+  store.undo() // (noop)
+  store.undo() // (noop)
+
+  store.redo() // 200
+  store.redo() // 300
+  store.redo() // (noop)
+  store.undo() // 200
+  store.undo() // 100
+  store.undo() // (noop)
+
+  t.same(out, [100, 200, 300, 200, 100, 200, 300, 200, 100])
+})
+
+test('redo+undo+update', t => {
+  const out = []
+  const store = createStoreAsStream()
+  store.getStream().subscribe(x => out.push(x))
+  store.update(100)
+  store.update(200)
+  store.update(300)
+  store.undo()
+  store.update(500)
+  store.redo()
+  store.undo()
+  store.undo()
+  store.redo()
+  t.same(out, [100, 200, 300, 200, 500, 200, 100, 200])
 })
