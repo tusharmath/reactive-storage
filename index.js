deb85085531265b71dd52593e9a406c27c5417a2
Tushar Mathur <tusharmath@gmail.com>
2016-04-06 15:01:28 +0530

    refactor(history): use histable for maintaining history

--- index.js
+++ index.js
@@ -3,67 +3,71 @@
  */
 
 'use strict'
+
 const BehaviorSubject = require('rx').BehaviorSubject
+const histable = require('histable')
 
 exports.create = exports.createStoreStream = (value, limit) => {
   limit = limit > 0 ? limit : 0
   const ignoredValues = {}
+  const isHistoryEnabled = limit > 0
+  var history = null
+
+  const initHistory = () => {
+    if (isHistoryEnabled) {
+      history = histable.create(limit)
+      history.push(value)
+    }
+  }
+  initHistory()
   if (value === undefined) {
     value = ignoredValues
   }
-  var UNDO_HISTORY = []
-  var REDO_HISTORY = []
 
   const subject = new BehaviorSubject(value)
   const dispatchValue = (_value, push) => {
     var isDefined = _value !== undefined
     var isDiff = value !== _value
-    var isPushable = push === true
-    var isNotIgnored = value !== ignoredValues
-    var isHistoryEnabled = limit > 0
-
-    if ([isHistoryEnabled, isDefined, !isPushable].every(Boolean)) {
-      REDO_HISTORY.push(value)
-    }
-
-    if ([isHistoryEnabled, isDefined, isDiff, isPushable, isNotIgnored].every(Boolean)) {
-      UNDO_HISTORY.push(value)
-    }
-
-    if ([isHistoryEnabled, isDefined, isDiff, isPushable, isNotIgnored, UNDO_HISTORY.length > limit].every(Boolean)) {
-      UNDO_HISTORY.shift()
-    }
 
     if ([isDefined, isDiff].every(Boolean)) {
       value = _value
       subject.onNext(value)
     }
   }
-  const reset = () => {
-    REDO_HISTORY = []
-    UNDO_HISTORY = []
-  }
 
   return {
     stream: subject.filter(x => x !== ignoredValues),
     set: cb => {
-      REDO_HISTORY = []
-      dispatchValue(typeof cb === 'function' ? cb(value) : cb, true)
+      const val = typeof cb === 'function' ? cb(value) : cb
+      dispatchValue(val)
+      if (isHistoryEnabled) {
+        history.push(val)
+      }
       return subject
     },
     end: () => {
-      reset()
+      initHistory()
       subject.onCompleted()
     },
     get: () => value,
-    undo: () => dispatchValue(UNDO_HISTORY.pop(), false),
-    redo: () => dispatchValue(REDO_HISTORY.pop(), true),
-    reset,
+    undo: () => {
+      if (isHistoryEnabled) {
+        dispatchValue(history.undo())
+      }
+    },
+    redo () {
+      if (isHistoryEnabled) {
+        dispatchValue(history.redo())
+      }
+    },
+    reset () {
+      initHistory()
+    },
     get canUndo () {
-      return UNDO_HISTORY.length > 0
+      return history.canUndo
     },
     get canRedo () {
-      return REDO_HISTORY.length > 0
+      return history.canRedo
     }
   }
 }
